f82ca1f376682e47091d1cd0d792c30501eb5071
wdw <wdw@82696.com>
2020-11-05 18:54:36 +0800

    quite calc

---
+++ js/quote_calc.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(async function() {
+
+    var toptip = new TopTip();
+
+    var pricePercent = [10, 8, 7, 6, 5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 
+                        -1, -1.5, -2, -2.5, -3, -3.5, -4, -5, -6, -7, -8, -10];
+
+
+
+    mask.hide();
+
+
+
+});
